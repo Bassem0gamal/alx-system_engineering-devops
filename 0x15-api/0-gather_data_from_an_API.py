4572fc6f186e93cb566bc88bc7ed7f096a533d2e
Basem Gamal <bassemgamalosman12@gmail.com>
2024-04-22 17:31:26 -0700

    Task 0 Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     todo_tasks = requests.get(
         url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed_task=[t.get("title")
-                    for t in todo_tasks if t.get("completed") is True]
+    completed_task = [t.get("title")
+                      for t in todo_tasks if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed_task), len(todo_tasks)))
     [print("\t {}".format(c)) for c in completed_task]
