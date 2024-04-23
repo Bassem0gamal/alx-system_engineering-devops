3d5b5a21f785c55923c312fbbead06b8f36c6f79
Basem Gamal <bassemgamalosman12@gmail.com>
2024-04-22 17:21:11 -0700

    Test

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A Python script that, using this REST API, returns information about his/her TODO list progress."""
+"""A Python script that, using this REST API,
+returns information about his / her TODO list progress."""
 
 import requests
 import sys
@@ -7,9 +8,11 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo_tasks = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todo_tasks = requests.get(
+        url + "todos", params = {"userId": sys.argv[1]}).json()
 
-    completed_task = [t.get("title") for t in todo_tasks if t.get("completed") is True]
+    completed_task = [t.get("title")
+                      for t in todo_tasks if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed_task), len(todo_tasks)))
     [print("\t {}".format(c)) for c in completed_task]
