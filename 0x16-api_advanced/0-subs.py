cbfda8940fe4af9b53c4914b5e1106ec2cb8a9b6
Basem Gamal <bassemgamalosman12@gmail.com>
2024-05-08 15:18:31 -0700

    Test2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,8 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json():
-        subs = data['data']['subscribers']
-        return subs
-    else:
+    if response.status_code == 404:
         return 0
+    
+    result = response.json().get("data")
+    return results.get("subscribers")
