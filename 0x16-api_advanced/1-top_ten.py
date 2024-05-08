858ac74b515021cbd1a6774f1cd4352ec4402643
Basem Gamal <bassemgamalosman12@gmail.com>
2024-05-08 15:25:05 -0700

    Test3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,13 +7,11 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts."""
     url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
     headers = {'user-agent': 'my-app/0.0.1'}
-    response = get(url, headers=headers, allow_redirects=False)
+    params = {"limit": 10}
+    response = get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 404:
         print("None")
-        return None
-    results = response.json().get("data")
-    children = results.get("children")
-    for child in children[:10]:
-        post = child.get("data")
-        print(post.get("title"))
+        return 
+    result = response.json().get("data")
+    [print(c.get("data").get("title")) for c in result.get("children")]
