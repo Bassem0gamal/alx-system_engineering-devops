b2e1a09f947e84d7dda2f14bdcf944d774a074cd
Basem Gamal <bassemgamalosman12@gmail.com>
2024-05-08 08:44:55 -0700

    Test

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet that find out why Apache is returning a 500 error and fix it.
+
+exec { 'Fix wordpress site':
+     command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+     provider => shell,
+     }
