130ab84b1a72fb56fdb201f03979ca849cdb8b7b
Basem Gamal <bassemgamalosman12@gmail.com>
2024-05-16 09:20:30 -0700

    Fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,11 @@
 # Increases the amount of traffix that Nginx can handle
 
 exec {'replace':
-  provider => shell,
-    command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-      before   => Exec['restart'],
-      }
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
 
 exec {'restart':
-  provider => shell,
-    command  => 'sudo service nginx restart',
-    }
+  command  => 'sudo service nginx restart',
+}
     
