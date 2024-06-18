6d2eeb4d08176ef04af142ebbed86c1e9c136a2b
BassemGamal <bassemgamalosman12@gmail.com>
2024-06-18 13:49:04 -0700

    0x16. API advanced: Task 1 Test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+
 import requests
 
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts listed for a given subreddit."""
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
+    params = {
+        "limit": 10
+    {
+
+    response = requests.get(url, headers=headers, allow_redirects=False, params=params)
+
     if response.status_code == 200:
         posts = response.json()['data']['children']
         for post in posts:
