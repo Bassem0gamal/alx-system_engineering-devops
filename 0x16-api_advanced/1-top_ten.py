0426c557eb9173f3d963ff2d2d5e25b013c33a27
Basem Gamal <121392526+Bassem0gamal@users.noreply.github.com>
2024-06-19 00:04:54 +0300

    Test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,14 @@ def top_ten(subreddit):
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
 
-    response = requests.get(url, headers=headers, allow_redirects=False, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                             params=params)
 
     if response.status_code == 404:
         print("None")
